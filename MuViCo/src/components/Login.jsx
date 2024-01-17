0b35b9886b86e1d94f75c97441b8a8939e26d5b1
Kalle Hahl <kalle.hahl@hotmail.com>
2024-01-17 10:24:50 +0200

    fixed eslint errors and cleaned login form

--- MuViCo/src/components/Login.jsx
+++ MuViCo/src/components/Login.jsx
@@ -1,35 +1,33 @@
-import { Form, Formik, useField, Field } from "formik";
+import { Form, Formik, Field } from "formik";
 
 const initialValues = {
-    username: '',
-    password: ''
-}
+  username: "",
+  password: "",
+};
 
-const LoginForm = ({onSubmit}) => {
-    const [usernameField] = useField('username')
-    const [passwordField] = useField('password')
-    return (
+const LoginForm = () => {
+  return (
     <>
-    <Form>
-        <Field type='text' name='username' placeholder='Username'/>
-        <Field type='password' name='password' placeholder='Password'/>
-        <button type='submit' text='login'>Login</button>
-    </Form>
+      <Form>
+        <Field type="text" name="username" placeholder="Username" />
+        <Field type="password" name="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </Form>
     </>
-    )
-}
+  );
+};
 
 const Login = () => {
-    const onSubmit = (values) => {
-        console.log(values.username,values.password)
-    }
-    return(
-        <div>
-            <Formik initialValues={initialValues} onSubmit={onSubmit}>
-                {({handleSubmit}) => <LoginForm onSubmit={handleSubmit}/>}
-            </Formik>
-        </div>
-    )
-}
+  const onSubmit = (values) => {
+    console.log(values.username, values.password);
+  };
+  return (
+    <div>
+      <Formik initialValues={initialValues} onSubmit={onSubmit}>
+        {() => <LoginForm />}
+      </Formik>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
