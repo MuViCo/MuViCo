f8ac8ee3e2575858a7a45dbf7bbfc4da13f56479
tfhuhtal <tuomas.huhtala@icloud.com>
2024-01-24 12:34:23 +0200

    fixes

--- src/config.js
+++
@@ -1,9 +0,0 @@
-export const inDevelopment = process.env.NODE_ENV === 'development'
-
-export const inStaging = process.env.STAGING === 'true'
-
-export const inProduction = !inStaging && process.env.NODE_ENV === 'production'
-
-export const inE2EMode = process.env.E2E === 'true'
-
-export const BASE_PATH = process.env.BASE_PATH || ''
