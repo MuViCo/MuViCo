b4c5f7fcf7341d19b23121eb5ebfc31e7666833a
Kalle Hahl <kalle.hahl@hotmail.com>
2024-01-26 15:49:42 +0200

    changes to file/folder structure and created a navbar and dropdown login

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client"
+import "bootstrap/dist/css/bootstrap.min.css"
 
-import App from "./client/App";
+import App from "./client/App"
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />)
