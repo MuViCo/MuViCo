df25f4b908e4f9f9064222004762614557fbaca8
niilolehtonen <niilo.i.lehtonen@gmail.com>
2024-01-26 14:46:46 +0200

    login & sign up routes connected with mongoose
    Co-authored-by: pmsainio <paul.m.sainio@gmail.com>

---
+++ src/server/models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+  username: String,
+  passwordHash: String,
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    // the passwordHash should not be revealed
+    delete returnedObject.passwordHash
+  },
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
--- src/server/routes/login.js
+++ src/server/routes/login.js
@@ -1,10 +1,32 @@
-const express = require("express");
+const jwt = require('jsonwebtoken')
+const express = require('express')
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+const config = require('../utils/config')
 
-const router = express.Router();
+const router = express.Router()
 
-router.post("/", (req, res) => {
-  const credentials = req.body;
-  res.send(JSON.stringify(credentials));
-});
+router.post('/', async (req, res) => {
+  const { username, password } = req.body
 
-module.exports = router;
+  const user = await User.findOne({ username })
+  const passwordCorrect =
+    user === null ? false : await bcrypt.compare(password, user.passwordHash)
+
+  if (!(user && passwordCorrect)) {
+    return res.status(401).json({
+      error: 'invalid username or password',
+    })
+  }
+
+  const userForToken = {
+    username: user.username,
+    id: user._id,
+  }
+
+  const token = jwt.sign(userForToken, config.SECRET)
+
+  res.status(200).send({ token, username: user.username, name: user.name })
+})
+
+module.exports = router
