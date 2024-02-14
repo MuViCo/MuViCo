8273e00e9aece22119e1c494050140c9eb6f1ab2
Kalle Hahl <kalle.hahl@hotmail.com>
2024-02-14 14:52:43 +0200

    added cypress and added cypress config file

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress")
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: "http://localhost:3000",
+    env: {
+      BACKEND: "http://localhost:8000/api",
+    },
+  },
+})
