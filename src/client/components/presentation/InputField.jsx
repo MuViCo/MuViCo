5fa8e49aa78a98b4b0255f8a76d206dd2eae593b
Kalle Hahl <kalle.hahl@hotmail.com>
2024-02-28 14:56:08 +0200

    added intital functionality for alerts

--- src/client/components/presentation/InputField.jsx
+++ src/client/components/presentation/InputField.jsx
@@ -1,12 +1,49 @@
 import React, { useState } from "react"
-import { VStack, FormControl, FormLabel, Input, Button } from "@chakra-ui/react"
+import {
+  VStack,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  useToast,
+} from "@chakra-ui/react"
+
+import * as yup from "yup"
+
+const videoInputSchema = yup.object().shape({
+  videoName: yup.string().required(),
+  videoUrl: yup.string().url().required(),
+})
 
 const InputField = ({ onAdd }) => {
   const [videoName, setVideoName] = useState("")
   const [videoUrl, setVideoUrl] = useState("")
+  const toast = useToast()
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault()
+
+    try {
+      await videoInputSchema.validate({ videoName, videoUrl })
+    } catch (error) {
+      toast({
+        position: "top",
+        title: "Validation Error",
+        status: "error",
+        description: error.message,
+        duration: 5000,
+        isClosable: true,
+      })
+      return
+    }
+    toast({
+      position: "top",
+      title: "Success!",
+      status: "success",
+      description: "Video added succesfully",
+      duration: 5000,
+      isClosable: true,
+    })
     onAdd({ videoName, videoUrl })
     setVideoName("")
     setVideoUrl("")
