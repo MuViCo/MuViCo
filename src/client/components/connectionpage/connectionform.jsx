e2583fcae61a386a8ffd98398bafb8e88ccfef81
niilolehtonen <niilo.i.lehtonen@gmail.com>
2024-03-06 15:10:24 +0200

    ip addresses are listed
    Co-authored-by: pmsainio <paul.m.sainio@gmail.com>

--- src/client/components/connectionpage/connectionform.jsx
+++ src/client/components/connectionpage/connectionform.jsx
@@ -1,21 +1,26 @@
 import { useState } from 'react'
 import { FormControl, FormLabel, Input, Button, Box } from "@chakra-ui/react"
 
-const ConnectionForm = ({ createConnection }) => {
-    const [ipAddress, setIpAddress] = useState('')
-    
-    const addConnection = (event) => {
-        event.preventDefault()
-        createConnection({
-            ipAddress: ipAddress,
-        })
+const ConnectionForm = () => {
+    const [ipAddress, setIpAddress] = useState('');
+    const [ips, setIps] = useState([]);
 
-        setIpAddress('')
-    }
-    
+    const addConnection = (event) => {
+        event.preventDefault();
+        console.log('Creating connection:', ipAddress);
+        setIps(ips.concat(ipAddress));
+        window.localStorage.setItem('ips', JSON.stringify(ips));
+        setIpAddress('');
+    };
+
     return (
         <Box>
             <h2>Connections</h2>
+            <div>
+                {ips.map((ip, index) => (
+                    <p key={index}>{ip}</p>
+                ))}
+            </div>
             <form onSubmit={addConnection}>
                 <FormControl>
                     <FormLabel htmlFor='ipAddress'>IP Address</FormLabel>
@@ -28,7 +33,7 @@ const ConnectionForm = ({ createConnection }) => {
                 <Button id='create-button' type="submit">Create Connection</Button>
             </form>
         </Box>
-    )
-}
+    );
+};
 
 export default ConnectionForm
