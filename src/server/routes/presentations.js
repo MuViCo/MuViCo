39a0aa6aed6c9a72dc271a18af523fb6a1f3d26e
Kalle Hahl <kalle.hahl@hotmail.com>
2024-03-12 16:03:26 +0200

    Fixed routiing for admin user

--- src/server/routes/presentations.js
+++ src/server/routes/presentations.js
@@ -1,8 +1,5 @@
 const express = require("express")
-const mongoose = require("mongoose")
-const jwt = require("jsonwebtoken")
 const { userExtractor } = require("../utils/middleware")
-const User = require("../models/user")
 const Presentation = require("../models/presentation")
 
 const router = express.Router()
@@ -13,9 +10,13 @@ const router = express.Router()
  */
 router.get("/", userExtractor, async (req, res) => {
   const user = req.user
-  const presentations = await Presentation.find({ user: user._id })
-  console.log("esitelmät: ", presentations)
-  res.json(presentations.map((presentation) => presentation.toJSON()))
+  if (user.isAdmin) {
+    const presentations = await Presentation.find()
+    res.json(presentations.map((presentation) => presentation.toJSON()))
+  } else {
+    const presentations = await Presentation.find({ user: user._id })
+    res.json(presentations.map((presentation) => presentation.toJSON()))
+  }
 })
 
 /**
