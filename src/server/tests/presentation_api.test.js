a5d92645149335268352ef2288e1250a5454ef0e
Kalle Hahl <kalle.hahl@hotmail.com>
2024-03-20 14:36:22 +0200

    added aws s3 mock library for testing and created tests for presentation route
    Co-authored-by: niilolehtonen <niilo.i.lehtonen@gmail.com>

---
+++ src/server/tests/presentation_api.test.js
@@ -0,0 +1,53 @@
+const supertest = require("supertest")
+const Presentation = require("../models/presentation")
+const User = require("../models/user")
+
+const app = require("../app")
+
+const api = supertest(app)
+
+let authHeader
+let testPresentationId
+
+describe("test presentation", () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+    await Presentation.deleteMany({})
+    await api
+      .post("/api/signup")
+      .send({ username: "testuser", password: "testpassword" })
+
+    const response = await api
+      .post("/api/login")
+      .send({ username: "testuser", password: "testpassword" })
+
+    authHeader = `Bearer ${response.body.token}`
+
+    await api
+      .post("/api/home")
+      .set("Authorization", authHeader)
+      .send({ name: "Test presentation" })
+    const presentation = await Presentation.findOne({
+      name: "Test presentation",
+    })
+    testPresentationId = presentation._id
+  })
+
+  describe("GET /api/presentation/:id", () => {
+    test("presentation is returned as json", async () => {
+      await api
+        .get(`/api/presentation/${testPresentationId}`)
+        .set("Authorization", authHeader)
+        .expect(200)
+        .expect("Content-Type", /application\/json/)
+    })
+  })
+  describe("DELETE", () => {
+    test(" /api/presentation/:id", async () => {
+      await api
+        .delete(`/api/presentation/${testPresentationId}`)
+        .set("Authorization", authHeader)
+        .expect(204)
+    })
+  })
+})
