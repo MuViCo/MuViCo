23aed7e96d8c2c841714342baec1a6f59ca17770
tfhuhtal <tuomas.huhtala@icloud.com>
2024-04-08 12:41:48 +0300

    removed the refresh bug

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -9,79 +9,65 @@ import NavBar from "./components/navbar"
 import FrontPage from "./components/frontpage"
 import HomePage from "./components/homepage"
 import PresentationPage from "./components/presentation"
-import presentationService from "./services/presentations"
 import ConnectionPage from "./components/connectionpage"
 import TermsPage from "./components/termspage"
 import UserMedia from "./components/admin/UserMedia"
 import UsersList from "./components/admin/UsersList"
 
 const App = () => {
-	const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null)
 
-	const [isInitialized, setIsInitialized] = useState(false)
+  const [isInitialized, setIsInitialized] = useState(false)
 
-	useEffect(() => {
-		const loggedUserJSON = window.localStorage.getItem("user")
-		if (loggedUserJSON) {
-			const parsedUser = JSON.parse(loggedUserJSON)
-			setUser(parsedUser)
-		}
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("user")
+    if (loggedUserJSON) {
+      const parsedUser = JSON.parse(loggedUserJSON)
+      setUser(parsedUser)
+    }
+    setIsInitialized(true)
+  }, [])
 
-		setIsInitialized(true)
+  if (!isInitialized) {
+    return <div>Loading...</div>
+  }
 
-		const handleUnload = () => {
-			// Poista käyttäjän tokeni sivun sulkemisen yhteydessä
-			window.localStorage.removeItem("user")
-		}
-
-		window.addEventListener("unload", handleUnload)
-
-		return () => {
-			// Poista tapahtumankäsittelijä komponentin purkamisen yhteydessä
-			window.removeEventListener("unload", handleUnload)
-		}
-	}, [])
-
-	if (!isInitialized) {
-		return <div>Loading...</div>
-	}
-
-	return (
-		<ChakraProvider theme={theme}>
-			<Fonts />
-			<Box>
-				<NavBar user={user} setUser={setUser} />
-				<Container pt={20} maxW="container.xl">
-					<Routes>
-						<Route path="/" element={<FrontPage />} />
-						<Route
-							path="/home"
-							element={user ? <HomePage user={user} /> : <Navigate to="/" />}
-						/>
-						<Route
-							path="/presentation/:id"
-							element={
-								user ? <PresentationPage userId={user.id} /> : <Navigate to="/" />
-							}
-						/>
-						<Route
-							path="/connections"
-							element={user ? <ConnectionPage /> : <Navigate to="/" />}
-						></Route>
-						<Route
-							path="/users"
-							element={user && user.isAdmin ? <UsersList /> : <Navigate to="/" />}
-						/>
-						<Route
-							path="/media"
-							element={user && user.isAdmin ? <UserMedia /> : <Navigate to="/" />}
-						/>
-						<Route path="/terms" element={<TermsPage />} />
-					</Routes>
-				</Container>
-			</Box>
-		</ChakraProvider>
-	)
+  return (
+    <ChakraProvider theme={theme}>
+      <Fonts />
+      <Box>
+        <NavBar user={user} setUser={setUser} />
+        <Container pt={20} maxW="container.xl">
+          <Routes>
+            <Route path="/" element={<FrontPage />} />
+            <Route
+              path="/home"
+              element={user ? <HomePage user={user} /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/presentation/:id"
+              element={
+                user ? <PresentationPage userId={user.id} /> : <Navigate to="/" />
+              }
+            />
+            <Route
+              path="/connections"
+              element={user ? <ConnectionPage /> : <Navigate to="/" />}
+            ></Route>
+            <Route
+              path="/users"
+              element={user && user.isAdmin ? <UsersList /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/media"
+              element={user && user.isAdmin ? <UserMedia /> : <Navigate to="/" />}
+            />
+            <Route path="/terms" element={<TermsPage />} />
+          </Routes>
+        </Container>
+      </Box>
+    </ChakraProvider>
+  )
 }
 
 export default App
