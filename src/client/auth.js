4149bec66f14d36952ce981b228595c84086a76e
viljavai <vilja.vaisanen@helsinki.fi>
2024-09-30 13:04:46 +0300

    lint errors fixed

--- src/client/auth.js
+++ src/client/auth.js
@@ -1,13 +1,13 @@
 const getToken = () => {
-  const user = window.localStorage.getItem("user");
-  return user ? JSON.parse(user).token : null;
+  const user = window.localStorage.getItem("user")
+  return user ? JSON.parse(user).token : null
 }
 
 // return true if token is expired othewise false
 const isTokenExpired = (token) => {
     if (!token) return true
     try {
-      const payload = JSON.parse(atob(token.split('.')[1]))
+      const payload = JSON.parse(atob(token.split(".")[1]))
       return payload.exp * 1000 < Date.now()
     } catch (e) {
       return true
