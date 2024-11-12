14d0fefda5d01736d66664560d899cc64e21da5d
lsommarberg <laura.sommarberg@gmail.com>
2024-11-12 12:48:14 +0200

    lint fix

--- src/client/components/utils/config.js
+++ src/client/components/utils/config.js
@@ -3,6 +3,6 @@ const firebaseConfig = {
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || process.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || process.env.VITE_FIREBASE_PROJECT_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID || process.env.VITE_FIREBASE_APP_ID,
-};
+}
 
-export default firebaseConfig;
+export default firebaseConfig
