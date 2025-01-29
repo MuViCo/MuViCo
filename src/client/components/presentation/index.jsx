8ae00d41f84ffcf0be16f4e8dc5cfbe2177adf60
Inkeri <inkeri.ahlstrom@helsinki.fi>
2025-01-29 11:48:45 +0200

    Formatting updates

--- src/client/components/presentation/index.jsx
+++ src/client/components/presentation/index.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react"
 import { useParams, useNavigate } from "react-router-dom"
 import { Button, Flex, Box, Text } from "@chakra-ui/react"
-import { fetchPresentationInfo, deletePresentation } from "../../redux/presentationReducer"
+import {
+  fetchPresentationInfo,
+  deletePresentation,
+} from "../../redux/presentationReducer"
 import "reactflow/dist/style.css"
 import { useDispatch, useSelector } from "react-redux"
 import ShowMode from "./ShowMode"
@@ -47,11 +50,10 @@ const PresentationPage = () => {
     const totalSize = presentationInfo.reduce((sum, cue) => {
       return cue.file ? sum + parseInt(cue.file.size) : sum
     }, 0)
-       if (presentationSize != (totalSize / (1024 * 1024)).toFixed(2)) {
+    if (presentationSize != (totalSize / (1024 * 1024)).toFixed(2)) {
       setPresentationSize((totalSize / (1024 * 1024)).toFixed(2))
     }
   }
-
 
   const handleDeletePresentation = (presentationId) => {
     setPresentationToDelete(presentationId)
@@ -81,33 +83,55 @@ const PresentationPage = () => {
 
   return (
     <>
-      { presentationInfo && (
-        <Box width="100vw" height="95vh" margin={0} padding={0} display="flex" flexDirection="column">
+      {presentationInfo && (
+        <Box
+          width="100vw"
+          height="95vh"
+          margin={0}
+          padding={0}
+          display="flex"
+          flexDirection="column"
+        >
           <Flex flexDirection="row" flexWrap="wrap" gap={4} padding={4}>
             <Button colorScheme="gray" onClick={handleShowMode}>
               {showMode ? "Edit mode" : "Show mode"}
             </Button>
             {!showMode && (
               <>
-                <Button colorScheme="gray" onClick={() => handleDeletePresentation(id)}>
+                <Button
+                  colorScheme="gray"
+                  onClick={() => handleDeletePresentation(id)}
+                >
                   Delete Presentation
                 </Button>
-                <Button colorScheme="gray" onClick={() => setIsToolboxOpen(true)}>
+                <Button
+                  colorScheme="gray"
+                  onClick={() => setIsToolboxOpen(true)}
+                >
                   Add Element
                 </Button>
               </>
             )}
-            <Text alignSelf="center" data-testid="presentationSize">{presentationSize} MB</Text>
+            <Text alignSelf="center" data-testid="presentationSize">
+              {presentationSize} MB
+            </Text>
           </Flex>
-          <Box flex="1" padding={4} marginLeft="0px" overflow="auto"> {/* Adjust marginLeft to move the grid to the left */}
-            {showMode && <ShowMode cues={presentationInfo}  />}
-            <EditMode id={id} cues={presentationInfo} isToolboxOpen={isToolboxOpen} setIsToolboxOpen={setIsToolboxOpen} />
+          <Box flex="1" padding={4} marginLeft="0px" overflow="auto">
+            {" "}
+            {/* Adjust marginLeft to move the grid to the left */}
+            {showMode && <ShowMode cues={presentationInfo} />}
+            <EditMode
+              id={id}
+              cues={presentationInfo}
+              isToolboxOpen={isToolboxOpen}
+              setIsToolboxOpen={setIsToolboxOpen}
+            />
           </Box>
           <Dialog
-          isOpen={isDialogOpen}
-          onClose={() => setIsDialogOpen(false)}
-          onConfirm={handleConfirmDelete}
-          message="Are you sure you want to delete this presentation?"
+            isOpen={isDialogOpen}
+            onClose={() => setIsDialogOpen(false)}
+            onConfirm={handleConfirmDelete}
+            message="Are you sure you want to delete this presentation?"
           />
         </Box>
       )}
