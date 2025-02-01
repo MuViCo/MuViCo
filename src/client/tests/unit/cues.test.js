eb4276ca01fb090899466a8adf6330754c7521e5
Inkeri <inkeri.ahlstrom@helsinki.fi>
2025-02-01 16:41:59 +0200

    Prettier formatting

--- src/client/tests/unit/cues.test.js
+++ src/client/tests/unit/cues.test.js
@@ -1,22 +1,22 @@
-import CuesForm from '../../components/presentation/Cues'
-import { render, screen, fireEvent, waitFor } from '@testing-library/react'
-import '@testing-library/jest-dom'
-import React from 'react'
-import { MemoryRouter } from 'react-router-dom'
+import CuesForm from "../../components/presentation/Cues"
+import { render, screen, fireEvent, waitFor } from "@testing-library/react"
+import "@testing-library/jest-dom"
+import React from "react"
+import { MemoryRouter } from "react-router-dom"
 
-describe('cues', () => {
-  test('render content', () => {
+describe("cues", () => {
+  test("render content", () => {
     render(
       <MemoryRouter>
         <CuesForm />
       </MemoryRouter>
     )
-    expect(screen.getByText('Screen 1-4*')).toBeDefined()
-    expect(screen.getByText('Index 1-100')).toBeDefined()
-    expect(screen.getByText('Name*')).toBeDefined()
+    expect(screen.getByText("Screen 1-4*")).toBeDefined()
+    expect(screen.getByText("Index 1-100")).toBeDefined()
+    expect(screen.getByText("Name*")).toBeDefined()
   })
 
-  test('add cues', async () => {
+  test("add cues", async () => {
     const addCue = jest.fn()
     const { getByLabelText, getByText, getByRole } = render(
       <MemoryRouter>
@@ -24,11 +24,11 @@ describe('cues', () => {
       </MemoryRouter>
     )
 
-    const indexInput = screen.getByRole('textbox')
+    const indexInput = screen.getByRole("textbox")
     fireEvent.change(indexInput, {
-      target: { value: 'testuser' },
+      target: { value: "testuser" },
     })
-    expect(indexInput).toHaveValue('testuser')
-    expect(screen.getByText('Submit')).toBeDefined()
+    expect(indexInput).toHaveValue("testuser")
+    expect(screen.getByText("Submit")).toBeDefined()
   })
 })
