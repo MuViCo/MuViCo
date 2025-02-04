5d809029b26be4d9f3133f198183aeeebb84901e
Inkeri <inkeri.ahlstrom@helsinki.fi>
2025-02-04 14:17:06 +0200

    Prettier formatting

--- src/client/tests/unit/toolbox.test.js
+++ src/client/tests/unit/toolbox.test.js
@@ -1,29 +1,50 @@
-import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-import Toolbox from '../../components/presentation/ToolBox.jsx';
-import '@testing-library/jest-dom'
+import React from "react"
+import { render, screen, fireEvent } from "@testing-library/react"
+import Toolbox from "../../components/presentation/ToolBox.jsx"
+import "@testing-library/jest-dom"
 
-describe('ToolBox Component', () => {
-  const mockAddCue = jest.fn();
-  const mockOnClose = jest.fn();
-  const position = { index: 1, screen: 1 };
+describe("ToolBox Component", () => {
+  const mockAddCue = jest.fn()
+  const mockOnClose = jest.fn()
+  const position = { index: 1, screen: 1 }
 
-  it('renders correctly when open', () => {
-    render(<Toolbox addCue={mockAddCue} isOpen={true} onClose={mockOnClose} position={position} />);
-    expect(screen.getByRole('dialog')).toBeInTheDocument();
-    expect(screen.getByTestId('close-drawer-button')).toBeInTheDocument();
-  });
+  it("renders correctly when open", () => {
+    render(
+      <Toolbox
+        addCue={mockAddCue}
+        isOpen={true}
+        onClose={mockOnClose}
+        position={position}
+      />
+    )
+    expect(screen.getByRole("dialog")).toBeInTheDocument()
+    expect(screen.getByTestId("close-drawer-button")).toBeInTheDocument()
+  })
 
-  it('calls onClose when the close button is clicked', () => {
-    render(<Toolbox addCue={mockAddCue} isOpen={true} onClose={mockOnClose} position={position} />);
+  it("calls onClose when the close button is clicked", () => {
+    render(
+      <Toolbox
+        addCue={mockAddCue}
+        isOpen={true}
+        onClose={mockOnClose}
+        position={position}
+      />
+    )
 
-    fireEvent.click(screen.getByTestId('close-drawer-button'));
-    expect(mockOnClose).toHaveBeenCalledTimes(1);
-  });
+    fireEvent.click(screen.getByTestId("close-drawer-button"))
+    expect(mockOnClose).toHaveBeenCalledTimes(1)
+  })
 
-  it('does not render when closed', () => {
-    render(<Toolbox addCue={mockAddCue} isOpen={false} onClose={mockOnClose} position={position} />);
+  it("does not render when closed", () => {
+    render(
+      <Toolbox
+        addCue={mockAddCue}
+        isOpen={false}
+        onClose={mockOnClose}
+        position={position}
+      />
+    )
 
-    expect(screen.queryByRole('dialog')).not.toBeInTheDocument();
-  });
-});
+    expect(screen.queryByRole("dialog")).not.toBeInTheDocument()
+  })
+})
