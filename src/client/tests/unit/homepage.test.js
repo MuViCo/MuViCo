952695ef1f14d95688c99eb4f5f4e5fc56bb276d
spherical-spinach <48479421+spherical-spinach@users.noreply.github.com>
2025-03-13 16:52:27 +0200

    Fix PresentationForm tests and remove duplicate tests

--- src/client/tests/unit/homepage.test.js
+++ src/client/tests/unit/homepage.test.js
@@ -7,9 +7,9 @@ import HomePage from "../../components/homepage/index"
 import {
   AdminControls,
   PresentationsGrid,
-  CreatePresentation,
 } from "../../components/homepage/index"
-import PresentationForm from "../../components/homepage/CreatePresentation"
+import PresentationFormWrapper from "../../components/homepage/PresentationFormWrapper"
+import PresentationForm from "../../components/homepage/PresentationForm"
 import presentationService from "../../services/presentations"
 import addInitialElements from "../../components/utils/addInitialElements"
 
@@ -85,13 +85,23 @@ describe("HomePage", () => {
 })
 
 describe("PresentationForm", () => {
+  test("Renders the 'New presentation'-button", () => {
+    render(
+      <PresentationFormWrapper
+        createPresentation={() => {}}
+        togglableRef={() => {}}
+        handleCancel={() => {}}
+      />
+    )
+    expect(screen.getByText("New presentation")).toBeInTheDocument()
+  })
   test("renders the form with name input field and buttons", () => {
     render(<PresentationForm />)
 
     expect(screen.getByRole("button", { name: /create/i })).toBeInTheDocument()
     expect(screen.getByRole("button", { name: /cancel/i })).toBeInTheDocument()
     expect(screen.getByTestId("presentation-name")).toBeInTheDocument()
-    expect(screen.getByLabelText("Name")).toBeInTheDocument()
+    expect(screen.getByLabelText("Name*")).toBeInTheDocument()
   })
 
   test("calls createPresentation function when create button is clicked", () => {
@@ -103,7 +113,7 @@ describe("PresentationForm", () => {
       />
     )
 
-    const nameInput = screen.getByLabelText("Name")
+    const nameInput = screen.getByLabelText("Name*")
     fireEvent.change(nameInput, { target: { value: "Test Presentation" } })
     fireEvent.click(screen.getByText("create"))
 
@@ -184,45 +194,5 @@ describe("PresentationsGrid", () => {
     )
     fireEvent.click(screen.getByText("Test Presentation"))
     expect(handlePresentationClickMock).toHaveBeenCalledWith("123")
-  })
-})
-
-describe("Create presentation form", () => {
-  test("renders the component", () => {
-    render(
-      <CreatePresentation
-        createPresentation={() => {}}
-        handleCancel={() => {}}
-        handleConnectionsClick={() => {}}
-      />
-    )
-    expect(screen.getByText("New presentation")).toBeInTheDocument()
-  })
-  test("renders the component with buttons", () => {
-    render(
-      <CreatePresentation
-        createPresentation={() => {}}
-        handleCancel={() => {}}
-        handleConnectionsClick={() => {}}
-      />
-    )
-    expect(screen.getByText("New presentation")).toBeInTheDocument()
-  })
-  test("calls createPresentation function when create button is clicked", () => {
-    const createPresentationMock = jest.fn()
-    render(
-      <CreatePresentation
-        createPresentation={createPresentationMock}
-        handleCancel={() => {}}
-        handleConnectionsClick={() => {}}
-      />
-    )
-    fireEvent.click(screen.getByText("New presentation"))
-    const nameInput = screen.getByLabelText("Name")
-    fireEvent.change(nameInput, { target: { value: "Test Presentation" } })
-    fireEvent.click(screen.getByText("create"))
-    expect(createPresentationMock).toHaveBeenCalledWith({
-      name: "Test Presentation",
-    })
   })
 })
