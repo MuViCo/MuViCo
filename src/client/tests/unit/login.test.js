9c3bda694970e911db24e6adc1f7893dd8e9a389
Inkeri <inkeri.ahlstrom@helsinki.fi>
2025-03-17 13:28:32 +0200

    Added handleKeyDown tests for Login form

--- src/client/tests/unit/login.test.js
+++ src/client/tests/unit/login.test.js
@@ -2,6 +2,7 @@ import React from "react"
 import { render, screen, fireEvent, waitFor } from "@testing-library/react"
 import "@testing-library/jest-dom"
 import { LoginForm } from "../../components/navbar/Login"
+import { describe } from "node:test"
 
 jest.mock("../../components/utils/firebase", () => ({
   apikey: "testkey",
@@ -93,6 +94,92 @@ describe("Login", () => {
     fireEvent.click(getByText("Log in"))
     await waitFor(() => {
       expect(screen.getByText("Password required")).toBeDefined()
+    })
+  })
+
+  test("error message when server error", async () => {
+    const mockSubmit = jest.fn().mockRejectedValue({
+      response: { data: { error: "Invalid credentials" } },
+    })
+
+    const { getByLabelText, getByText } = render(
+      <LoginForm onSubmit={mockSubmit} />
+    )
+
+    fireEvent.change(getByLabelText("Username"), {
+      target: { value: "testuser" },
+    })
+    fireEvent.change(getByLabelText("Password"), {
+      target: { value: "testpassword" },
+    })
+
+    fireEvent.click(getByText("Log in"))
+
+    await waitFor(() => {
+      expect(screen.getByText("Invalid credentials")).toBeDefined()
+    })
+  })
+})
+
+describe("HandleKeyDown", () => {
+  test("handleKeyDown shifts focus correctly from username", () => {
+    const onSubmit = jest.fn()
+    const { getByLabelText } = render(<LoginForm onSubmit={onSubmit} />)
+    const usernameInput = getByLabelText("Username")
+    const passwordInput = getByLabelText("Password")
+
+    fireEvent.keyDown(usernameInput, { key: "Tab" })
+    expect(document.activeElement).toBe(passwordInput)
+  })
+  test("handleKeyDown shifts focus correctly from password", () => {
+    const onSubmit = jest.fn()
+    const { getByLabelText } = render(<LoginForm onSubmit={onSubmit} />)
+    const passwordInput = getByLabelText("Password")
+    const submitButton = screen.getByTestId("login_inform")
+
+    fireEvent.keyDown(passwordInput, { key: "Tab" })
+    expect(document.activeElement).toBe(submitButton)
+  })
+  test("handleKeyDown calls handleSubmit when pressing enter on password", () => {
+    const onSubmit = jest.fn()
+    const { getByLabelText } = render(<LoginForm onSubmit={onSubmit} />)
+    const usernameInput = getByLabelText("Username")
+    const passwordInput = getByLabelText("Password")
+
+    fireEvent.change(usernameInput, { target: { value: "testuser" } })
+    fireEvent.change(passwordInput, { target: { value: "testpassword" } })
+
+    fireEvent.keyDown(passwordInput, { key: "Enter" })
+
+    expect(onSubmit).toHaveBeenCalledWith({
+      username: "testuser",
+      password: "testpassword",
+    })
+  })
+  test("handleKeyDown shifts focus correctly from submit", () => {
+    const onSubmit = jest.fn()
+    const { getByLabelText } = render(<LoginForm onSubmit={onSubmit} />)
+    const usernameInput = getByLabelText("Username")
+    const submitButton = screen.getByTestId("login_inform")
+
+    fireEvent.keyDown(submitButton, { key: "Tab" })
+    expect(document.activeElement).toBe(usernameInput)
+  })
+  test("handleKeyDown calls handleSubmit when pressing enter on submit button", () => {
+    const onSubmit = jest.fn()
+    const { getByLabelText } = render(<LoginForm onSubmit={onSubmit} />)
+    const usernameInput = getByLabelText("Username")
+    const passwordInput = getByLabelText("Password")
+    const submitButton = screen.getByTestId("login_inform")
+
+    fireEvent.change(usernameInput, { target: { value: "testuser" } })
+    fireEvent.change(passwordInput, { target: { value: "testpassword" } })
+
+    fireEvent.keyDown(submitButton, { key: "Enter" })
+
+    expect(onSubmit).toHaveBeenCalledWith({
+      username: "testuser",
+      password: "testpassword",
     })
   })
 })
