b2c20f68aa54f271fdfd859784688c42a35e44b8
sannilatvala <119106675+sannilatvala@users.noreply.github.com>
2025-03-28 15:07:55 +0200

    save url

--- src/server/models/presentation.js
+++ src/server/models/presentation.js
@@ -20,6 +20,7 @@ const presentationSchema = mongoose.Schema({
         id: String,
         name: String,
         url: String,
+        driveId: String,
         size: {type: String, default: "0"},
         type: {type: String, default: "image/jpeg"}
       },
