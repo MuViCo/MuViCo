99c02cc1e6ecf593f5668286a239ed40feaf7792
Elisa Suorsa <84924097+xelmas@users.noreply.github.com>
2025-04-12 09:46:35 +0300

    add homepageData, presentationPageData, FeatureSection and NestedList components - Added homepageData.js and presentationPageData.js for managing manual content. - Created FeatureSection.jsx and NestedList.jsx for rendering feature lists with nested items. - Updated HomepageManual and PresentationManual to use the new data files and components. - Added larger fontSize to UserManualModal.jsx header

--- src/client/components/presentation/PresentationManual.jsx
+++ src/client/components/presentation/PresentationManual.jsx
@@ -1,34 +1,58 @@
-import { Text, List, ListItem } from "@chakra-ui/react"
+import { Text } from "@chakra-ui/react"
+import {
+  editModeFeaturesData,
+  stepsToAddElementData,
+  editingElementData,
+  showModeFeaturesData,
+} from "./presentationPageData"
+import FeatureSection from "../utils/FeatureSection"
 
 const PresentationManual = () => {
   return (
     <>
       <Text mb={4}>
         Welcome to the user manual. This modal provides guidance on how to use
-        the application.
+        the edit and show mode.
       </Text>
-      <Text fontWeight="bold" mb={1}>
-        Presentation page
+      <Text fontWeight="bold" fontSize="2xl" mb={1}>
+        Presentation editor page
       </Text>
       <Text mb={3}>
-        After signing in, you will see the home page where you manage your
-        presentations.
+        Here you will see all the elements in the current presentation.
       </Text>
-      <Text fontWeight="bold" mb={1}>
-        Features on this page:
+      <FeatureSection
+        title="Features on this page"
+        data={editModeFeaturesData}
+      ></FeatureSection>
+
+      <Text fontWeight="bold" mb={1} mt={10} fontSize="2xl">
+        Adding an element
       </Text>
-      <List spacing={2} mb={3} styleType="disc" pl={4}>
-        <ListItem>
-          New presentation button: click this to create a new presentation.
-        </ListItem>
-        <ListItem>
-          Logout button: Click this to log out of your account.
-        </ListItem>
-      </List>
-      <Text fontWeight="bold" mb={1}>
-        Some title
+      <FeatureSection
+        title="Steps to add an element"
+        data={stepsToAddElementData}
+        listAs="ol"
+        listStyleType="inside"
+      ></FeatureSection>
+
+      <FeatureSection
+        title="Editing an existing element"
+        data={editingElementData}
+      ></FeatureSection>
+
+      <Text fontWeight="bold" mb={3} mt={10} fontSize="2xl">
+        Show mode page
       </Text>
-      <Text mb={3}>some text</Text>
+      <Text mb={2}>
+        When you click on the Show mode button on the edit presentation page you
+        will be directed to the show mode page which allows you to preview and
+        control your presentation in real-time. Here, you can navigate through
+        different screens and slides (indexes).
+      </Text>
+      <FeatureSection
+        title="Features on this page:"
+        data={showModeFeaturesData}
+      ></FeatureSection>
     </>
   )
 }
