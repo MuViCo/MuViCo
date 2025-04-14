877373751937ada38181cd106a9af831c105a89c
Elisa Suorsa <84924097+xelmas@users.noreply.github.com>
2025-04-14 12:48:55 +0300

    Transfer homePageData.js and presentationPageData.js to own data folder and update imports

--- src/client/components/homepage/HomepageManual.jsx
+++ src/client/components/homepage/HomepageManual.jsx
@@ -1,5 +1,5 @@
 import { Text } from "@chakra-ui/react"
-import { homefeaturesData } from "./homepageData"
+import { homefeaturesData } from "../data/homepageData"
 import FeatureSection from "../utils/FeatureSection"
 
 const HomepageManual = () => {
