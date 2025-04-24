3935ee717006d26c418cc37bd09540d13d8d6069
sannilatvala <119106675+sannilatvala@users.noreply.github.com>
2025-04-24 16:38:03 +0300

    added routes for linking and unlinking drive
    Co-authored-by: ainkeri <inkeri.ahlstrom@helsinki.fi>

---
+++ src/server/routes/users.js
@@ -0,0 +1,42 @@
+const express = require("express")
+const { userExtractor } = require("../utils/middleware")
+const router = express.Router()
+
+router.post("/link-drive", userExtractor, async (req, res) => {
+  try {
+    const { driveAccessToken } = req.body
+    const user = req.user
+
+    user.driveToken = driveAccessToken
+    await user.save()
+
+    res.status(200).json({
+      message: "Google Drive linked successfully",
+      username: user.username,
+      driveToken: user.driveToken,
+    })
+  } catch (error) {
+    console.error("Error linking Drive:", error)
+    res.status(500).json({ error: "Failed to link Google Drive" })
+  }
+})
+
+router.post("/unlink-drive", userExtractor, async (req, res) => {
+  try {
+    const user = req.user
+
+    user.driveToken = null
+    await user.save()
+
+    res.status(200).json({
+      message: "Google Drive unlinked successfully",
+      username: user.username,
+      driveToken: user.driveToken,
+    })
+  } catch (error) {
+    console.error("Error unlinking Drive:", error)
+    res.status(500).json({ error: "Failed to link Google Drive" })
+  }
+})
+
+module.exports = router
