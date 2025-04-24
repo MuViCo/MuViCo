098e0f5fecc3c1028c193730b22ff1019b2b2a94
sannilatvala <119106675+sannilatvala@users.noreply.github.com>
2025-04-24 16:16:48 +0300

    added storage field to presentationSchema and driveId to cues files
    Co-authored-by: ainkeri <inkeri.ahlstrom@helsinki.fi>

--- src/server/models/presentation.js
+++ src/server/models/presentation.js
@@ -11,6 +11,12 @@ const presentationSchema = mongoose.Schema({
     ref: "User",
   },
 
+  storage: {
+    type: String,
+    required: true,
+    default: "aws",
+  },
+
   cues: [
     {
       index: { type: Number, required: true },
@@ -20,6 +26,7 @@ const presentationSchema = mongoose.Schema({
         id: String,
         name: String,
         url: String,
+        driveId: String,
         size: { type: String, default: "0" },
         type: { type: String, default: "image/jpeg" },
       },
