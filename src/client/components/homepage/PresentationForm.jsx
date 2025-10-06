c622dc3b57752395d51833edad97b7ac1d39b124
Aada Nwajei <aada.nwajei@gmail.com>
2025-10-06 12:32:08 +0300

    Add dropdown menu for choosing starting frame color

--- src/client/components/homepage/PresentationForm.jsx
+++ src/client/components/homepage/PresentationForm.jsx
@@ -6,21 +6,25 @@ import {
   Button,
   Box,
   Flex,
+  Select,
 } from "@chakra-ui/react"
 
 const PresentationForm = ({ createPresentation, onCancel }) => {
   const [name, setName] = useState("")
   const [screenCount, setScreenCount] = useState(1)
+  const [startingFrameColor, setStartingFrameColor] = useState("black")
 
   const addPresentation = (event) => {
     event.preventDefault()
     createPresentation({
       name,
-      screenCount: parseInt(screenCount, 10)
+      screenCount: parseInt(screenCount, 10),
+      startingFrameColor
     })
 
     setName("")
     setScreenCount(1)
+    setStartingFrameColor("black")
     onCancel()
   }
 
@@ -50,6 +54,19 @@ const PresentationForm = ({ createPresentation, onCancel }) => {
             value={screenCount}
             onChange={({ target }) => setScreenCount(target.value)}
           />
+          
+          <FormLabel fontWeight="bold" style={{ marginTop: ".5em" }}>
+            Starting Frame Color
+          </FormLabel>
+          <Select 
+            data-testid="starting-frame-color"
+            value={startingFrameColor} 
+            onChange={(e) => setStartingFrameColor(e.target.value)}
+            placeholder="Select color"
+          >
+            <option value="black">Black</option>
+            <option value="white">White</option>
+          </Select>
         </FormControl>
         <Flex align="center" mt={2} mb={4}>
           <Button id="create-button" type="submit" colorScheme="purple">
