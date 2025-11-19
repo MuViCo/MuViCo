03974ba924ef9f81fba48774dc5ac027b05189fa
Aada Nwajei <aada.nwajei@gmail.com>
2025-11-19 12:07:53 +0200

    Add help page to frontpage

---
+++ src/client/components/data/frontpageData.js
@@ -0,0 +1,14 @@
+export const frontpageFeaturesData = [
+    {
+        title: "Presentations button: click this to go to the presentations page.",
+        items: [],
+    },
+    {
+        title: "Logout button: Click this to log out of your account.",
+        items: [],
+    },
+    {
+        title: "Theme button (🌙/☀️): Adjust the theme between light and dark modes.",
+        items: [],
+    },
+]
