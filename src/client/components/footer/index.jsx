c76cc797400b232e98b301e05f95a32a3be58837
BjornKhermik10 <bjorn.khermik@helsinki.fi>
2026-02-15 13:26:10 +0200

    Replace version import with __APP_VERSION__

--- src/client/components/footer/index.jsx
+++ src/client/components/footer/index.jsx
@@ -1,11 +1,10 @@
 import { Box } from "@chakra-ui/react"
 import { Link } from "react-router-dom"
 import { useState, useEffect } from "react"
-import { version } from "../../../../package.json"
 
 const Footer = () => (
   <Box p="4" textAlign="center" opacity={0.4} fontSize="sm">
-    &copy; {new Date().getFullYear()} MuViCo v{version}. All rights reserved. 
+    &copy; {new Date().getFullYear()} MuViCo v{__APP_VERSION__}. All rights reserved.
     <Link to="https://github.com/MuViCo/MuViCo">https://github.com/MuViCo/MuViCo</Link>
   </Box>
 )
