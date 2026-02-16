5b25b714448c48437f663bec5c850bf5c6ea5882
Karaatsar <kara.atsar@helsinki.fi>
2026-02-16 13:26:19 +0200

    remove unnessesary dropdown arrow

--- src/client/components/homepage/PresentationForm.jsx
+++ src/client/components/homepage/PresentationForm.jsx
@@ -58,22 +58,22 @@ const PresentationForm = ({ createPresentation, onCancel }) => {
           <FormLabel fontWeight="bold" style={{display:"none", marginTop: ".5em" }}>
             Starting Frame Color
           </FormLabel>
-          <Select style={{display:"none"}}
-            data-testid="starting-frame-color"
+          {/* <Select style={{display:"none"}} */}
+            {/* data-testid="starting-frame-color"
             value={startingFrameColor} 
             onChange={(e) => setStartingFrameColor(e.target.value)}
             placeholder="Select color"
-          >
+          
             <option value="#000000" style={{backgroundColor: "yellow", color: "white"}}>Black</option>
             <option value="white" style={{backgroundColor: "white", color: "black"}}>White</option>
             <option value="indigo" style={{backgroundColor: "#0c2bde", color: "white"}}>Indigo</option>
-            <option value="tropicalindigo" style={{backgroundColor: "#e5ed9f", color: "black"}}>Tropical indigo</option>
+            <option value="tropicalindigo" style={{backgroundColor: "#e5ed9f", color: "black"}}>Tropical indigo</option> */}
            {/* <ColorPickerWithPresets
             color={startingFrameColor}
             onChange={setStartingFrameColor}
             presetColors={presetColors}
           />  */}
-          </Select>
+          {/* </Select> */}
         </FormControl>
         <Flex align="center" mt={2} mb={4}>
           <Button id="create-button" type="submit" colorScheme="purple">
