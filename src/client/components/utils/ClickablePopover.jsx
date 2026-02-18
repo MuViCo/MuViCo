43f6eecfe2e3d0738193361d8f6d5a3fcb296a90
LauriSakari <lauri.harjukoski@gmail.com>
2026-02-18 14:05:06 +0200

    Add clickable popover component for help text

---
+++ src/client/components/utils/ClickablePopover.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import { Popover, PopoverTrigger, PopoverContent, PopoverBody, useDisclosure } from "@chakra-ui/react"
+
+const ClickablePopover = ({ children, label, ...popoverProps }) => {
+  const { isOpen, onToggle, onClose } = useDisclosure()
+
+  return (
+    <Popover isOpen={isOpen} onClose={onClose} {...popoverProps}>
+      <PopoverTrigger>
+        {React.cloneElement(children, {
+          onClick: onToggle
+        })}
+      </PopoverTrigger>
+      <PopoverContent>
+        <PopoverBody>{label}</PopoverBody>
+      </PopoverContent>
+    </Popover>
+  )
+}
+
+export default ClickablePopover
