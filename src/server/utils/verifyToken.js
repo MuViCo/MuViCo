5ff4506e417ee3bf11ecc80812f0fe65f7ee9384
Lauri Kaijansinkko <lauri.kaijansinkko@helsinki.fi>
2026-03-02 17:16:17 +0200

    Moved firebase servicekey out of aws secrets

--- src/server/utils/verifyToken.js
+++ src/server/utils/verifyToken.js
@@ -1,37 +1,16 @@
 const admin = require("firebase-admin")
-const {
-  SecretsManagerClient,
-  GetSecretValueCommand,
-} = require("@aws-sdk/client-secrets-manager")
 
-const { BUCKET_REGION, ACCESS_KEY, SECRET_ACCESS_KEY } = require("./config")
+const { FIREBASE_SERVICE_KEY } = require("./config")
 
 const initializeFirebase = async () => {
-  const secret_name = "ServiceAccountKey"
-
-  const client = new SecretsManagerClient({
-    region: BUCKET_REGION,
-    credentials: {
-      accessKeyId: ACCESS_KEY,
-      secretAccessKey: SECRET_ACCESS_KEY,
-    },
-  })
-
-  const response = await client.send(
-    new GetSecretValueCommand({
-      SecretId: secret_name,
-      VersionStage: "AWSCURRENT",
-    })
-  )
-
-  const secret = JSON.parse(response.SecretString)
-
-  if (!admin.apps.length) {
-    admin.initializeApp({
-      credential: admin.credential.cert(secret),
-    })
-    console.log("Firebase initialized successfully.")
+  if (admin.apps.length) {
+    return
   }
+  const service_account = JSON.parse(atob(FIREBASE_SERVICE_KEY))
+  admin.initializeApp({
+    credential: admin.credential.cert(service_account),
+  })
+  console.log("Firebase initialized successfully")
 }
 
 const verifyToken = async (req, res, next) => {
