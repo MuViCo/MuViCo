8ab6463cfdd01664ceefdc2cdfe0eb1f28f445cb
Lauri Kaijansinkko <lauri.kaijansinkko@helsinki.fi>
2026-03-03 15:43:20 +0200

    removed unused code

--- src/client/services/auth.js
+++ src/client/services/auth.js
@@ -1,15 +1,9 @@
 import axios from "axios"
 import getToken from "../auth"
 
-const authUrl = "/api/auth"
 const loginUrl = "/api/login"
 const signupUrl = "/api/signup"
 const changePasswordUrl = "/api/users/change-password"
-
-const auth = async () => {
-  const response = await axios.get(authUrl)
-  return response.data
-}
 
 const login = async (credentials) => {
   const response = await axios.post(loginUrl, credentials)
@@ -30,4 +24,4 @@ const changepassword = async (credentials) => {
   return response.data
 }
 
-export default { auth, login, signup, changepassword }
+export default { login, signup, changepassword }
