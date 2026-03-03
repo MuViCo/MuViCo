0e43f16a1b84be2d9ae91c13f325b30801300d78
Lauri Kaijansinkko <lauri.kaijansinkko@helsinki.fi>
2026-03-03 14:54:36 +0200

    Add source map to client build The code is open source so might as well include them for easier debugging

--- vite.config.js
+++ vite.config.js
@@ -29,6 +29,7 @@ export default defineConfig({
   },
   build: {
     manifest: true,
+    sourcemap: true,
   },
   define: {
     "process.env": {
@@ -36,10 +37,6 @@ export default defineConfig({
       VITE_FIREBASE_AUTH_DOMAIN: process.env.VITE_FIREBASE_AUTH_DOMAIN,
       VITE_FIREBASE_PROJECT_ID: process.env.VITE_FIREBASE_PROJECT_ID,
       VITE_FIREBASE_APP_ID: process.env.VITE_FIREBASE_APP_ID,
-      VITE_FIREBASE_STORAGE_BUCKET: process.env.VITE_FIREBASE_STORAGE_BUCKET,
-      VITE_FIREBASE_MESSAGING_SENDER_ID:
-        process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-      VITE_FIREBASE_MEASUREMENT_ID: process.env.VITE_FIREBASE_MEASUREMENT_ID,
     },
     __APP_VERSION__: JSON.stringify(pkg.version),
   },
