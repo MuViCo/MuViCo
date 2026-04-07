be7ecc446e75b00329b41ded8d52e25055eca02e
Sandra Ole <sandra.ole@hotmail.fi>
2026-04-07 15:09:08 +0300

    move button configuration to shared constants and use it in EditModeContainer.jsx

---
+++ src/client/constants/buttonConfigs.js
@@ -0,0 +1,23 @@
+export const buttonLabels = {
+	settingsAriaLabel: "Presentation Settings",
+	showMode: "Go to Show Mode",
+	tutorial: "Tutorial",
+}
+
+export const buttonStyles = {
+	settings: {
+		colorScheme: "purple",
+		size: "sm",
+		p: "6px",
+	},
+	showMode: {
+		colorScheme: "purple",
+		size: "sm",
+	},
+	tutorial: {
+		marginLeft: "auto",
+		colorScheme: "purple",
+		variant: "solid",
+		size: "sm",
+	},
+}
