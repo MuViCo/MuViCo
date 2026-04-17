fc7eb944b184ebe22a15fc90e1774b3e93a37419
Lauri Kaijansinkko <lauri.kaijansinkko@helsinki.fi>
2026-04-17 20:05:37 +0300

    Added jest-mongodb to run db in memory for tests

--- src/server/app.js
+++ src/server/app.js
@@ -28,14 +28,19 @@ morgan.token("data", (req, res) => {
   }
 })
 
-mongoose
-  .connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info("connected to MongoDB")
-  })
-  .catch((error) => {
-    logger.error("error connection to MongoDB:", error.message)
-  })
+if (process.env.NODE_ENV === "test" && global.__MONGO_URI__) {
+  console.log("Connecting to test DB:", global.__MONGO_URI__)
+  mongoose.connect(global.__MONGO_URI__)
+} else {
+  mongoose
+    .connect(config.MONGODB_URI)
+    .then(() => {
+      logger.info("connected to MongoDB")
+    })
+    .catch((error) => {
+      logger.error("error connection to MongoDB:", error.message)
+    })
+}
 
 app.use(cors())
 app.use(express.json())
