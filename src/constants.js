a080e8dc8447a641881abb2cbf00667d66a76e1f
Matias Rinne <matias.rinne@helsinki.fi>
2026-04-21 10:17:31 +0300

    Revert constants.js export and imports to use ES module style

--- src/constants.js
+++ src/constants.js
@@ -1,36 +1,24 @@
 /* password validation constants */
 
-const minPwLength = 8
-const maxPwLength = 72
+export const minPwLength = 8
+export const maxPwLength = 72
 
-const invalidPwCharRegex =
+export const invalidPwCharRegex =
   /[^a-zA-Z0-9 !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~äöåÄÖÅ]+/
 
 
 /* username validation constants */
 
-const minUsernameLength = 3
-const maxUsernameLength = 30
+export const minUsernameLength = 3
+export const maxUsernameLength = 30
 
-const usernameAllowedCharsRegex = /^[A-Za-z0-9._-]+$/ // Username is limited to letters, numbers, dot, underscore and hyphen
+export const usernameAllowedCharsRegex = /^[A-Za-z0-9._-]+$/ // Username is limited to letters, numbers, dot, underscore and hyphen
 
-const usernameStartEndRegex = /^[A-Za-z0-9](?:[A-Za-z0-9._-]*[A-Za-z0-9])?$/ // Username must start and end with alphanumeric characters
+export const usernameStartEndRegex = /^[A-Za-z0-9](?:[A-Za-z0-9._-]*[A-Za-z0-9])?$/ // Username must start and end with alphanumeric characters
 
-const usernameConsecutiveSpecialsRegex = /[._-]{2,}/ // Prevent repetitive separators
+export const usernameConsecutiveSpecialsRegex = /[._-]{2,}/ // Prevent repetitive separators
 
 
 /* bcrypt salt rounds */
 
-const saltRounds = 10
-
-module.exports = {
-  minPwLength,
-  maxPwLength,
-  invalidPwCharRegex,
-  minUsernameLength,
-  maxUsernameLength,
-  usernameAllowedCharsRegex,
-  usernameStartEndRegex,
-  usernameConsecutiveSpecialsRegex,
-  saltRounds,
-}
+export const saltRounds = 10
