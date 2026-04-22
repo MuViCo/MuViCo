5aac2026d7a0dbc7d2e7284f756737f5ceb2b1f4
Karaatsar <kara.atsar@helsinki.fi>
2026-04-22 13:49:17 +0300

    add comments to server/models

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -1,6 +1,17 @@
+/**
+ * User model definition for MuViCo application
+ * Each user has a unique username and may have multiple presentations
+ * Passwords are stored as hashes for security
+ * Users can authenticate via Firebase or traditional username/password
+ * Admin users have elevated permissions for managing the application
+ * This model is used by the user controller and routes to interact with the database
+ * when creating, updating, retrieving, and deleting users and their associated presentations.
+ */
+
 const mongoose = require("mongoose")
 const uniqueValidator = require("mongoose-unique-validator")
 
+// Define the user schema for authentication and profile data
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -35,6 +46,7 @@ userSchema.set("toJSON", {
 
 userSchema.plugin(uniqueValidator)
 
+// Compile and export the User model
 const User = mongoose.model("User", userSchema)
 
 module.exports = User
